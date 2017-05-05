c58178c9e1cd8b971a345159a97a75a86cbb9d4f
DavidWallJ <djkindread@gmail.com>
2017-05-05 19:49:04 +0800

    shortened paths time to test heroku again

--- server.js
+++ server.js
@@ -16,11 +16,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(cors());
 
-// MONGOLAB_URI="mongodb://DavidJWall:Meiguanxi8@ds127101.mlab.com:27101/url-shortener";
-
 // connect to mongodb via mongoose
 // first option is mongo on Heroku and the second is mongo locally
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/urlShortener');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost');
 
 
 var regex =/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
